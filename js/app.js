faa998eb98996dd1727c27d7b4a61739c49d48dc
Hamilton Limeira <hamiltondosanjos@gmail.com>
2020-11-03 14:24:30 -0300

    Efeito 3d para flexbox

---
+++ js/app.js
@@ -0,0 +1,44 @@
+//Movimento 3d
+const card       = document.querySelector('.card');
+const  container = document.querySelector('.container');
+//Itens
+const title = document.querySelector('.title');
+const sneaker = document.querySelector('.sneaker img');
+const circle = document.querySelector('.circle');
+const purchase = document.querySelector('.purchase button');
+const description = document.querySelector('.info h3');
+const sizes = document.querySelector('.sizes');
+
+//Evento de movimento
+container.addEventListener('mousemove', (e) => {
+    let xAxis = (window.innerWidth  / 2 -e.pageX) / 10;
+    let yAxis = (window.innerHeight / 2 -e.pageY) / 10;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Animação entrada
+container.addEventListener("mouseenter", (e) => {
+    card.style.transition = "none";
+    //Popout
+    title.style.transform = "translateZ(150px)";
+    sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+    description.style.transform = "translateZ(125px)";
+    circle.style.transform = "translateZ(120px)";
+    sizes.style.transform = "translateZ(100px)";
+    purchase.style.transform = "translateZ(75px)";
+  });
+  //Animação Saída
+  container.addEventListener("mouseleave", (e) => {
+    card.style.transition = "all 0.5s ease";
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+    //Popback
+    title.style.transform = "translateZ(0px)";
+    sneaker.style.transform = "translateZ(200px) rotateZ(0deg)";
+    circle.style.transform = "translateZ(0px)";
+    description.style.transform = "translateZ(0px)";
+    sizes.style.transform = "translateZ(0px)";
+    purchase.style.transform = "translateZ(0px)";
+
+
+
+
+  });
